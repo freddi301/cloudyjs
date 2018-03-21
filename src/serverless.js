4fcd4f21b2d6674a9968694645b8a10e9a3bc3c8
Frederik Batuna <gobi301@gmail.com>
2018-03-21 16:44:23 +0100

    fix: aws lambda

--- src/serverless.js
+++ src/serverless.js
@@ -1,21 +1,26 @@
 // @flow
 
-require("babel-register");
+import TestApp from "./testApp";
+import { client } from "./rest";
 
-const app = new (require("../src/testApp").default);
-const client = require("../src/rest").client;
+const app = new TestApp();
 
-module.exports.work = (event: any, context: any, callback: any) => {
-  const req = JSON.parse(event.body);
-  const result = Promise.resolve(
-    (app: any)[req.function].call(client({ app: app, url: `http://${event.headers.Host}${event.path}` }), req.argument)
-  );
-  result
-    .then(result => {
-      callback(null, {
-        statusCode: 200,
-        body: result
-      });
-    })
-    .catch(error => callback(error));
+export const work = async (event: any, context: any, callback: any) => {
+  try {
+    const body = JSON.parse(event.body);
+    const clientInstance = client({
+      app,
+      url: `http://${event.headers.Host}${event.path}`
+    });
+    const result = await (app: any)[body.function].call(
+      clientInstance,
+      body.argument
+    );
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    });
+  } catch (error) {
+    callback(error);
+  }
 };
